d8d190089384df7fb3841288e8f8f75415e25212
Migorithm <saka90030@gmail.com>
2024-08-22 00:13:17 +0900

    remove escape

--- src/commands.rs
+++ src/commands.rs
@@ -6,7 +6,7 @@ pub(crate) enum Command {
 impl TryFrom<&[u8]> for Command {
     fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
         match value {
-            b"*1\\r\\n$4\\r\\nPING\r\n" => Ok(Command::Ping),
+            b"*1\r\n$4\r\nPING\r\n" => Ok(Command::Ping),
             e => {
                 eprintln!("Unrecognized command: {:?}", e);
                 Err(Self::Error::UnrecognizedCommand)
