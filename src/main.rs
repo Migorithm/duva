e260b04518de499fa960dcf7070d2934cad02c26
Migo <saka90030@gmail.com>
2024-10-20 15:46:01 +0400

    implementation change from std to tokio

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,44 @@
+use tokio::{
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
 pub mod commands;
 pub mod error;
 
-use std::{
-    io::{BufRead, Write},
-    net::TcpListener,
-};
-
-fn main() {
+#[tokio::main]
+async fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
-                // buffered reader
-                let mut reader = std::io::BufReader::new(&stream);
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        tokio::spawn(async move {
+            process(socket).await;
+        });
+    }
+}
 
-                let mut lines = reader.lines();
-                while let Some(Ok(line)) = lines.next() {
-                    println!("Received {:?}", line);
-                    match commands::Command::try_from(line.as_str()) {
-                        Ok(commands::Command::Ping) => {
-                            (&stream).write_all(b"+PONG\r\n").unwrap();
-                        }
-                        Err(e) => {
-                            println!("error: {:?}", e);
-                            continue;
-                        }
-                    }
-                }
+async fn process(stream: TcpStream) {
+    println!("accepted new connection");
+    // buffered reader
+    let (read_half, mut writer) = stream.into_split();
+
+    let reader = BufReader::new(read_half);
+
+    let mut lines = reader.lines();
+
+    while let Ok(Some(line)) = lines.next_line().await {
+        println!("Received {:?}", line);
+        match commands::Command::try_from(line.as_str()) {
+            Ok(commands::Command::Ping) => {
+                writer.write(b"+PONG\r\n").await.unwrap();
             }
             Err(e) => {
-                println!("error: {}", e);
+                println!("error: {:?}", e);
+                continue;
             }
         }
     }
