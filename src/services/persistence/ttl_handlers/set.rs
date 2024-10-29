26733277f4be1533dc3cdc64eb08f379637e9355
Migo <saka90030@gmail.com>
2024-10-29 21:32:08 +0400

    expire extracting logic moved to ttl command

--- src/services/persistence/ttl_handlers/set.rs
+++ src/services/persistence/ttl_handlers/set.rs
@@ -1,7 +1,4 @@
-use std::{
-    cmp::Reverse,
-    time::{Duration, SystemTime},
-};
+use std::cmp::Reverse;
 
 use tokio::sync::mpsc::Receiver;
 
@@ -12,11 +9,9 @@ use super::{command::TtlCommand, pr_queue};
 async fn set_ttl_actor(mut recv: Receiver<TtlCommand>) -> Result<()> {
     while let Some(command) = recv.recv().await {
         let mut queue = pr_queue().write().await;
-        let (expire_in_mills, key) = match command {
-            TtlCommand::Expiry { expiry, key } => (expiry, key),
-            TtlCommand::StopSentinel => break,
+        let Some((expire_at, key)) = command.get_expiration() else {
+            break;
         };
-        let expire_at = SystemTime::now() + Duration::from_millis(expire_in_mills);
         queue.push((Reverse(expire_at), key));
     }
     Ok(())
