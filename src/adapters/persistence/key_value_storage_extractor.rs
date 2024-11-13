6e947fc586a2b4fbb2b597e4f155b7a9664d208b
Eunchan Cho <eunchan.cho@talabat.com>
2024-11-13 21:08:58 +0400

    Remove vector when it reads

--- src/adapters/persistence/key_value_storage_extractor.rs
+++ src/adapters/persistence/key_value_storage_extractor.rs
@@ -1,10 +1,11 @@
-use super::size_encoding::data_decode;
+use std::panic;
+use super::size_encoding::string_decode;
 
 /// # Extract Key-Value Pair Storage
 /// Extract key-value pair from the data buffer and remove the extracted data from the buffer.
-/// 
+///
 /// Each key-value pair is stored as follows:
-/// 
+///
 /// 1. Optional Expire Information:
 /// - **Timestamp in Seconds:**
 /// ```
@@ -19,15 +20,15 @@ use super::size_encoding::data_decode;
 /// 2. **Value Type:** 1-byte flag indicating the type and encoding of the value.
 /// 3. **Key:** String encoded.
 /// 4. **Value:** Encoding depends on the value type.
-/// 
-/// # Arguments 
-/// 
+///
+/// # Arguments
+///
 /// * `data`: &mut Vec<u8>
-/// 
-/// returns: Option<(key: String, value: String, expiry_time: Option<u64>)> 
-/// 
-/// # Examples 
-/// 
+///
+/// returns: Option<(key: String, value: String, expiry_time: Option<u64>)>
+///
+/// # Examples
+///
 /// ```
 /// #[test]
 /// fn test_non_expiry_key_value_pair() {
@@ -39,7 +40,7 @@ use super::size_encoding::data_decode;
 ///     assert_eq!(expiry_time, None);
 ///     assert_eq!(data.len(), 0);
 /// }
-/// 
+///
 /// #[test]
 /// fn test_with_milliseconds_expiry_key_value_pair() {
 ///     let mut data:Vec<u8> = vec![0xFC, 0x15, 0x72, 0xE7, 0x07, 0x8F, 0x01, 0x00, 0x00, 0x00, 0x03, 0x62, 0x61, 0x7A, 0x03, 0x71, 0x75, 0x78];
@@ -50,7 +51,7 @@ use super::size_encoding::data_decode;
 ///     assert!(expiry_time.is_some());
 ///     assert_eq!(data.len(), 0);
 /// }
-/// 
+///
 /// #[test]
 /// fn test_with_seconds_expiry_key_value_pair() {
 ///     let mut data:Vec<u8> = vec![0xFD, 0x52, 0xED, 0x2A, 0x66, 0x00, 0x03, 0x62, 0x61, 0x7A, 0x03, 0x71, 0x75, 0x78];
@@ -66,49 +67,52 @@ pub fn extract_key_value_expiry(data : &mut Vec<u8>) -> Option<(String, String, 
     let mut key = String::new();
     let mut value = String::new();
     let mut expiry_time = None;
-    let mut i = 0;
-    while i < data.len() {
-        match data[i] {
+    while data.len() > 0 {
+        println!("{}", data[0]);
+        match data[0] {
             0xFC => {
-                i += 1;
-                let expiry = extract_expiry_time_in_milliseconds(data.as_slice(), i);
-                expiry_time = Some(expiry);
-                i += 8;
+                data.remove(0);
+                expiry_time = extract_expiry_time_in_milliseconds(data);
             }
             0xFD => {
-                i += 1;
-                let expiry = extract_expiry_time_in_seconds(data.as_slice(), i);
-                expiry_time = Some(expiry);
-                i += 4;
+                data.remove(0);
+                expiry_time = extract_expiry_time_in_seconds(data);
             }
-            00 => {
-                i += 1;
-                let key_data = data_decode(&data[i..])?;
-                key = key_data.data ;
-                i += key_data.byte_length;
-                let value_data = data_decode(&data[i..])?;
+            0x00 => {
+                data.remove(0);
+                let key_data = string_decode(data)?;
+                key = key_data.data;
+                let value_data = string_decode(data)?;
                 value = value_data.data;
-                i += value_data.byte_length;
             }
             _ => {
                 return None;
             }
         }
     }
-    data.drain(..i);
     Some((key, value, expiry_time))
 }
 
-fn extract_expiry_time_in_seconds(data: &[u8], start_pos: usize) -> u64{
-    let end_pos = start_pos  + 4;
-    let slice: &[u8] = &data[start_pos ..end_pos];
-    u32::from_le_bytes(slice.try_into().unwrap()).into()
+fn extract_expiry_time_in_seconds(data: &mut Vec<u8>) -> Option<u64>{
+    let end_pos = 4;
+    let slice: &[u8] = &data[..end_pos];
+    if let Ok(result) = panic::catch_unwind(|| u32::from_le_bytes(slice.try_into().unwrap()).into()){
+        data.drain(..end_pos);
+        Some(result)
+    } else {
+        None
+    }
 }
 
-fn extract_expiry_time_in_milliseconds(data: &[u8], start_pos: usize) -> u64 {
-    let end_pos = start_pos + 8;
-    let slice: &[u8] = &data[start_pos..end_pos];
-    u64::from_le_bytes(slice.try_into().unwrap()).into()
+fn extract_expiry_time_in_milliseconds(data: &mut Vec<u8>) -> Option<u64>{
+    let end_pos =  8;
+    let slice: &[u8] = &data[..end_pos];
+    if let Ok(result) = panic::catch_unwind(|| u64::from_le_bytes(slice.try_into().unwrap()).into()){
+        data.drain(..end_pos);
+        Some(result)
+    } else {
+        None
+    }
 }
 
 #[test]
