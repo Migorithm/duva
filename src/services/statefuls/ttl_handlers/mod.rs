f2f5ea6bf798c139e6ec32048185654a53d91b87
Migo <saka90030@gmail.com>
2024-11-25 20:49:07 +0400

    remove glocal pr queue

--- src/services/statefuls/ttl_handlers/mod.rs
+++ src/services/statefuls/ttl_handlers/mod.rs
@@ -1,12 +1,1 @@
-use std::{cmp::Reverse, collections::BinaryHeap, sync::OnceLock, time::SystemTime};
-
-use tokio::sync::RwLock;
-
-pub mod delete_actor;
 pub mod set;
-
-static TTL_QUEUE: OnceLock<RwLock<BinaryHeap<(Reverse<SystemTime>, String)>>> = OnceLock::new();
-
-fn ttl_queue() -> &'static RwLock<BinaryHeap<(Reverse<SystemTime>, String)>> {
-    TTL_QUEUE.get_or_init(|| RwLock::new(BinaryHeap::new()))
-}
