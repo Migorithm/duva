0206cbadfa5804829cf723fd9e030c5b7883b0c2
Eunchan Cho <eunchan.cho@talabat.com>
2024-12-02 12:43:32 +0400

    extract method
    Signed-off-by: Eunchan Cho <eunchan.cho@talabat.com>

--- src/adapters/persistence/encoder/encoding_processor.rs
+++ src/adapters/persistence/encoder/encoding_processor.rs
@@ -2,6 +2,7 @@ use crate::adapters::persistence::byte_encoder::{encode_checksum, encode_databas
 use crate::services::interfaces::endec::Processable;
 use crate::services::statefuls::routers::save_actor::SaveActorCommand;
 use crate::services::CacheEntry;
+use anyhow::Result;
 use std::collections::VecDeque;
 use tokio::io::AsyncWriteExt;
 
@@ -19,7 +20,7 @@ pub(in crate::adapters::persistence) struct EncodingMeta {
 }
 
 impl Processable for EncodingProcessor {
-    async fn add_meta(&mut self) -> anyhow::Result<()> {
+    async fn add_meta(&mut self) -> Result<()> {
         let meta = [
             encode_header("0011")?,
             encode_metadata(Vec::from([("redis-ver", "6.0.16")]))?,
@@ -29,7 +30,7 @@ impl Processable for EncodingProcessor {
         self.file.write_all(&meta.concat()).await?;
         Ok(())
     }
-    async fn handle_cmd(&mut self, cmd: SaveActorCommand) -> anyhow::Result<bool> {
+    async fn handle_cmd(&mut self, cmd: SaveActorCommand) -> Result<bool> {
         match cmd {
             SaveActorCommand::LocalShardSize {
                 total_size,
@@ -52,23 +53,13 @@ impl Processable for EncodingProcessor {
                     self.meta.chunk_queue.push_back(chunk);
                 } else {
                     self.meta.chunk_queue.push_back(chunk);
-                    while let Some(chunk) = self.meta.chunk_queue.pop_front() {
-                        for kvs in chunk {
-                            let encoded_chunk = kvs.encode_with_key()?;
-                            self.file.write_all(&encoded_chunk).await?;
-                        }
-                    }
+                    self.encode_chunk_queue().await?;
                 }
             }
             SaveActorCommand::StopSentinel => {
                 self.meta.num_of_cache_actors -= 1;
                 if self.meta.num_of_cache_actors == 0 {
-                    while let Some(chunk) = self.meta.chunk_queue.pop_front() {
-                        for kvs in chunk {
-                            let encoded_chunk = kvs.encode_with_key()?;
-                            self.file.write_all(&encoded_chunk).await?;
-                        }
-                    }
+                    self.encode_chunk_queue().await?;
                     let checksum = encode_checksum(&[0; 8])?;
                     self.file.write_all(&checksum).await?;
                     return Ok(true);
@@ -76,6 +67,18 @@ impl Processable for EncodingProcessor {
             }
         }
         Ok(false)
+    }
+}
+
+impl EncodingProcessor {
+    async fn encode_chunk_queue(&mut self) -> Result<()> {
+        while let Some(chunk) = self.meta.chunk_queue.pop_front() {
+            for kvs in chunk {
+                let encoded_chunk = kvs.encode_with_key()?;
+                self.file.write_all(&encoded_chunk).await?;
+            }
+        }
+        Ok(())
     }
 }
 
