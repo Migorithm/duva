9ac398e7398ddb383b179e403f1d9949720bc349
Migo <saka90030@gmail.com>
2025-02-06 01:05:50 +0400

    test:test_cluster_forget_node_propagation

--- tests/test_cluster_forget.rs
+++ tests/test_cluster_forget.rs
@@ -50,3 +50,31 @@ async fn test_cluster_forget_node_return_error_when_wrong_id_given() {
     // THEN
     assert_eq!(cluster_info, array(vec!["cluster_known_nodes:0"]));
 }
+
+#[tokio::test]
+async fn test_cluster_forget_node_propagation() {
+    // GIVEN
+    const HOP_COUNT: usize = 0;
+    let master_p = spawn_server_process();
+    let repl_p = spawn_server_as_slave(&master_p);
+    let repl_p2 = spawn_server_as_slave(&master_p);
+    let mut processes = vec![master_p, repl_p, repl_p2];
+
+    check_internodes_communication(&mut processes, HOP_COUNT, 2).unwrap();
+
+    // WHEN
+    const TIMEOUT: u64 = 2;
+    let mut client_handler = ClientStreamHandler::new(processes[0].bind_addr()).await;
+    let replica_id = processes[1].bind_addr();
+    let cmd = &array(vec!["cluster", "forget", &replica_id]);
+    client_handler.send_and_get(cmd).await;
+    let never_arrivable_msg = processes[1].heartbeat_msg(1);
+
+    // THEN - master_p and repl_p2 doesn't get message from repl_p2
+
+    let res1 = processes[0].timed_wait_for_message(&never_arrivable_msg, HOP_COUNT, TIMEOUT);
+    let res2 = processes[2].timed_wait_for_message(&never_arrivable_msg, HOP_COUNT, TIMEOUT);
+
+    assert!(res1.is_err());
+    assert!(res2.is_err());
+}
