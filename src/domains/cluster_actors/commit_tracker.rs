5ca54a08eb9e04739c9d31469dc5c11e6f9bae98
Migo <saka90030@gmail.com>
2025-02-23 22:12:57 +0400

    rm: unused

--- src/domains/cluster_actors/commit_tracker.rs
+++
@@ -1,6 +0,0 @@
-use std::collections::HashMap;
-
-use crate::domains::peers::identifier::PeerIdentifier;
-
-#[derive(Default)]
-pub struct CommitTracker(HashMap<PeerIdentifier, u64>); // commit index
