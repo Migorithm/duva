6182e21e6c86c6ed0bf0f972a46296d7171d55fe
Migo <saka90030@gmail.com>
2025-03-25 06:50:09 +0400

    take-rm, method name articulated

--- src/domains/cluster_actors/consensus/mod.rs
+++ src/domains/cluster_actors/consensus/mod.rs
@@ -1,6 +1,6 @@
 pub mod voting;
 use super::commands::ConsensusClientResponse;
-use crate::make_smart_pointer;
+use crate::{domains::peers::identifier::PeerIdentifier, make_smart_pointer};
 
 use std::collections::HashMap;
 use tokio::sync::oneshot::Sender;
@@ -26,11 +26,17 @@ impl LogConsensusTracker {
             },
         );
     }
-    pub(crate) fn take(
-        &mut self,
-        offset: &u64,
-    ) -> Option<ConsensusVoting<Sender<ConsensusClientResponse>>> {
-        self.0.remove(offset)
+
+    pub(crate) fn track_progress(&mut self, log_idx: u64, from: PeerIdentifier) {
+        if let Some(mut consensus) = self.remove(&log_idx) {
+            if consensus.votable(&from) {
+                println!("[INFO] Received acks for log index num: {}", log_idx);
+                consensus.increase_vote(from);
+            }
+            if let Some(consensus) = consensus.send_result_or_pending(log_idx) {
+                self.insert(log_idx, consensus);
+            }
+        }
     }
 }
 make_smart_pointer!(LogConsensusTracker, HashMap<u64, ConsensusVoting<Sender<ConsensusClientResponse>>>);
--- src/domains/cluster_actors/consensus/voting.rs
+++ src/domains/cluster_actors/consensus/voting.rs
@@ -12,9 +12,6 @@ pub struct ConsensusVoting<T> {
 }
 impl<T> ConsensusVoting<T> {
     pub(crate) fn increase_vote(&mut self, voter: PeerIdentifier) {
-        if self.voters.iter().any(|v| v == &voter) {
-            return;
-        }
         self.cnt += 1;
         self.voters.push(voter);
     }
@@ -22,10 +19,13 @@ impl<T> ConsensusVoting<T> {
     fn get_required_votes(&self) -> u8 {
         ((self.voters.capacity() as f64 + 1.0) / 2.0).ceil() as u8
     }
+    pub(crate) fn votable(&self, voter: &PeerIdentifier) -> bool {
+        !self.voters.iter().any(|v| v == voter)
+    }
 }
 
 impl ConsensusVoting<ReplicationVote> {
-    pub(crate) fn maybe_not_finished(self, log_index: u64) -> Option<Self> {
+    pub(crate) fn send_result_or_pending(self, log_index: u64) -> Option<Self> {
         if self.cnt >= self.get_required_votes() {
             let _ = self.callback.send(ConsensusClientResponse::LogIndex(Some(log_index)));
             None
