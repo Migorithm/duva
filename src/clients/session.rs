332e583fd7507d8e13b2eb516379b3539f7f5133
Migo <saka90030@gmail.com>
2025-03-26 20:29:32 +0400

    feat: register client

--- src/clients/session.rs
+++ src/clients/session.rs
@@ -2,11 +2,21 @@ use chrono::{DateTime, Utc};
 use std::collections::HashMap;
 use uuid::Uuid;
 
-use crate::domains::query_parsers::QueryIO;
+use crate::{domains::query_parsers::QueryIO, make_smart_pointer};
 
 struct ClientSessions(HashMap<Uuid, Session>);
 
+#[derive(Default)]
 struct Session {
     last_accessed: DateTime<Utc>,
-    response: QueryIO,
+    response: Option<QueryIO>,
+}
+make_smart_pointer!(ClientSessions,HashMap<Uuid, Session>);
+
+impl ClientSessions {
+    pub(crate) fn register_client(&mut self) -> Uuid {
+        let id = Uuid::now_v7();
+        self.insert(id, Session::default());
+        id
+    }
 }
