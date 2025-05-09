81ba81c578e927229222cdf36976c71c89edce10
Migo <saka90030@gmail.com>
2025-05-09 21:43:28 +0400

    simplify test on heartbeat

--- duva/tests/cluster_ops/test_cluster_known_nodes_increase_when_new_replica_is_added.rs
+++ duva/tests/cluster_ops/test_cluster_known_nodes_increase_when_new_replica_is_added.rs
@@ -1,21 +1,17 @@
-use crate::common::{Client, ServerEnv, spawn_server_process};
+use crate::common::{Client, ServerEnv, form_cluster, spawn_server_process};
 
 fn run_cluster_topology_change_when_new_node_added(with_append_only: bool) -> anyhow::Result<()> {
     // GIVEN
-    let env = ServerEnv::default().with_append_only(with_append_only);
-    let mut leader_p = spawn_server_process(&env, true)?;
+    let mut env = ServerEnv::default().with_append_only(with_append_only);
+    let mut repl_env = ServerEnv::default().with_append_only(with_append_only);
+
+    // Form cluster with leader and replica
+    let [leader_p, _] = form_cluster(&mut [&mut env, &mut repl_env], true);
 
     let cmd = "cluster info";
 
-    let repl_env = ServerEnv::default()
-        .with_bind_addr(leader_p.bind_addr())
-        .with_append_only(with_append_only);
-    let mut repl_p = spawn_server_process(&repl_env, true)?;
-    repl_p.wait_for_message(&leader_p.heartbeat_msg(0))?;
-    leader_p.wait_for_message(&repl_p.heartbeat_msg(0))?;
-
     let mut client_handler = Client::new(leader_p.port);
-    let cluster_info = client_handler.send_and_get(cmd.as_bytes(), 1);
+    let cluster_info = client_handler.send_and_get(cmd, 1);
     assert_eq!(cluster_info, vec!["cluster_known_nodes:1".to_string()]);
 
     // WHEN -- new replica is added
@@ -26,10 +22,10 @@ fn run_cluster_topology_change_when_new_node_added(with_append_only: bool) -> an
     new_repl_p.wait_for_message(&leader_p.heartbeat_msg(0))?;
 
     //THEN
-    let cluster_info = client_handler.send_and_get(cmd.as_bytes(), 1);
+    let cluster_info = client_handler.send_and_get(cmd, 1);
     assert_eq!(cluster_info, vec!["cluster_known_nodes:2".to_string()]);
 
-    let nodes = client_handler.send_and_get("cluster nodes".as_bytes(), 3);
+    let nodes = client_handler.send_and_get("cluster nodes", 3);
     assert_eq!(nodes.len(), 3);
 
     let mut leader_nodes = Vec::new();
