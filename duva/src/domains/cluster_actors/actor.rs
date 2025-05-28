7084caaae1c7aaf316a1390b3dec2813b08b5c50
Migo <saka90030@gmail.com>
2025-05-28 09:49:46 +0400

    test: start rebalance unittest

--- duva/src/domains/cluster_actors/actor.rs
+++ duva/src/domains/cluster_actors/actor.rs
@@ -2000,4 +2000,83 @@ pub mod test {
         assert!(msg.is_some());
         assert_eq!(msg.unwrap(), QueryIO::StartRebalance);
     }
+
+    #[tokio::test]
+    async fn test_start_rebalance_before_connection_is_made() {
+        // GIVEN
+        let mut cluster_actor = cluster_actor_create_helper().await;
+
+        // WHEN
+        cluster_actor.start_rebalance(PeerIdentifier("127.0.0.1:6559".into())).await;
+
+        // THEN
+        // No pending requests should be created since the member is not connected
+        assert!(cluster_actor.pending_requests.is_none());
+        // No message should be sent to the peer
+    }
+
+    #[tokio::test]
+    async fn test_start_rebalance_happy_path() {
+        // GIVEN
+        let mut cluster_actor = cluster_actor_create_helper().await;
+        let buf = FakeReadWrite::new();
+        let (peer_id, peer) = create_peer_helper(
+            cluster_actor.self_handler.clone(),
+            0,
+            &ReplicationId::Key(uuid::Uuid::now_v7().to_string()), // non data
+            6559,
+            NodeKind::NonData,
+            buf.clone(),
+        );
+
+        cluster_actor.add_peer(peer).await;
+
+        // WHEN
+        cluster_actor.start_rebalance(peer_id).await;
+
+        // THEN
+        assert!(cluster_actor.pending_requests.is_some());
+        let msg = buf.0.lock().await.pop_front();
+        assert!(msg.is_some());
+        let hb = msg.unwrap();
+        assert!(matches!(hb, QueryIO::ClusterHeartBeat(..)));
+
+        let QueryIO::ClusterHeartBeat(hb) = hb else {
+            panic!("Expected ClusterHeartBeat message");
+        };
+        assert!(hb.hashring.is_some());
+        assert_eq!(cluster_actor.hash_ring.get_pnode_count(), 2);
+        assert_eq!(cluster_actor.hash_ring, hb.hashring.unwrap());
+    }
+
+    #[tokio::test]
+    async fn test_start_rebalance_should_be_idempotent() {
+        // GIVEN
+        let mut cluster_actor = cluster_actor_create_helper().await;
+        let buf = FakeReadWrite::new();
+        let (peer_id, peer) = create_peer_helper(
+            cluster_actor.self_handler.clone(),
+            0,
+            &ReplicationId::Key(uuid::Uuid::now_v7().to_string()), // non data
+            6559,
+            NodeKind::NonData,
+            buf.clone(),
+        );
+
+        cluster_actor.add_peer(peer).await;
+        // WHEN
+        cluster_actor.start_rebalance(peer_id.clone()).await;
+        assert_eq!(cluster_actor.hash_ring.get_pnode_count(), 2);
+        cluster_actor.start_rebalance(peer_id).await;
+
+        // THEN
+        assert_eq!(cluster_actor.hash_ring.get_pnode_count(), 2);
+
+        // ! still, the message should be sent
+        let msg1 = buf.0.lock().await.pop_front();
+        let msg2 = buf.0.lock().await.pop_front();
+        assert!(msg1.is_some());
+        assert!(msg2.is_some());
+        assert_eq!(msg1, msg2);
+    }
 }
