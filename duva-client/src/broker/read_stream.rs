81ee24d36ba824b4b7a6690b431a95fe102fd713
echo724 <echo.backend@gmail.com>
2025-06-23 22:28:04 +0400

    feat: manage multi leader connections with main peer_id

--- duva-client/src/broker/read_stream.rs
+++ duva-client/src/broker/read_stream.rs
@@ -5,12 +5,14 @@ use duva::{
 };
 
 use crate::broker::BrokerMessage;
+use duva::prelude::PeerIdentifier;
 
 pub struct ServerStreamReader(pub(crate) OwnedReadHalf);
 impl ServerStreamReader {
-    pub fn run(
+    pub fn run_with_id(
         mut self,
         controller_sender: tokio::sync::mpsc::Sender<BrokerMessage>,
+        peer_id: PeerIdentifier,
     ) -> oneshot::Sender<()> {
         let (kill_trigger, kill_switch) = tokio::sync::oneshot::channel::<()>();
 
@@ -21,26 +23,26 @@ impl ServerStreamReader {
                 match self.0.read_values().await {
                     | Ok(query_ios) => {
                         for query_io in query_ios {
-                            if controller_sender
-                                .send(BrokerMessage::FromServer(Ok(query_io)))
-                                .await
-                                .is_err()
-                            {
+                            let message =
+                                BrokerMessage::FromServerWithId(peer_id.clone(), Ok(query_io));
+                            if controller_sender.send(message).await.is_err() {
                                 break;
                             }
                         }
                     },
                     | Err(IoError::ConnectionAborted) | Err(IoError::ConnectionReset) => {
-                        let _ = controller_sender
-                            .send(BrokerMessage::FromServer(Err(IoError::ConnectionAborted)))
-                            .await;
-                        println!("Connection reset or aborted");
+                        let message = BrokerMessage::FromServerWithId(
+                            peer_id.clone(),
+                            Err(IoError::ConnectionAborted),
+                        );
+                        let _ = controller_sender.send(message).await;
+                        println!("Connection reset or aborted for peer: {}", peer_id);
                         break;
                     },
 
                     | Err(e) => {
-                        if controller_sender.send(BrokerMessage::FromServer(Err(e))).await.is_err()
-                        {
+                        let message = BrokerMessage::FromServerWithId(peer_id.clone(), Err(e));
+                        if controller_sender.send(message).await.is_err() {
                             break;
                         }
                     },
