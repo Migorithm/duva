4255a46a0cb0e295766906b23a7ca2b6af5ae0f7
echo724 <echo.backend@gmail.com>
2025-06-29 22:47:21 +0400

    feat: modify topology to have node info WIP

--- duva/src/domains/cluster_actors/topology.rs
+++ duva/src/domains/cluster_actors/topology.rs
@@ -1,14 +1,43 @@
 use crate::domains::cluster_actors::hash_ring::HashRing;
+use crate::domains::cluster_actors::replication::{
+    ReplicationId, ReplicationRole, ReplicationState,
+};
+use crate::domains::peers::peer::PeerState;
 use crate::prelude::PeerIdentifier;
 
 #[derive(bincode::Encode, bincode::Decode, Debug, PartialEq, Clone, Default)]
 pub struct Topology {
-    pub connected_peers: Vec<PeerIdentifier>,
+    pub node_infos: Vec<NodeReplInfo>,
     pub hash_ring: HashRing,
 }
 
 impl Topology {
-    pub fn new(connected_peers: Vec<PeerIdentifier>, hash_ring: HashRing) -> Self {
-        Self { connected_peers, hash_ring }
+    pub fn new(node_info: Vec<NodeReplInfo>, hash_ring: HashRing) -> Self {
+        Self { node_infos: node_info, hash_ring }
+    }
+}
+
+#[derive(bincode::Encode, bincode::Decode, Debug, PartialEq, Clone)]
+pub struct NodeReplInfo {
+    pub peer_id: PeerIdentifier,
+    pub repl_id: ReplicationId,
+    pub repl_role: ReplicationRole,
+}
+
+impl NodeReplInfo {
+    pub fn from_peer_state(peer_state: &PeerState) -> Self {
+        Self {
+            peer_id: peer_state.id().clone(),
+            repl_id: peer_state.replid.clone(),
+            repl_role: peer_state.role.clone(),
+        }
+    }
+
+    pub fn from_replication_state(replication_state: &ReplicationState) -> Self {
+        Self {
+            peer_id: replication_state.self_identifier(),
+            repl_id: replication_state.replid.clone(),
+            repl_role: replication_state.role.clone(),
+        }
     }
 }
