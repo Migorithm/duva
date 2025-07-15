c20e4b044e1077ead7dcfc0869e80f80b6c6b021
echo724 <echo.backend@gmail.com>
2025-07-15 22:29:27 +0400

    test: add test cases

---
+++ duva/tests/client_ops/test_route_del.rs
@@ -0,0 +1,49 @@
+use std::{thread::sleep, time::Duration};
+
+use crate::common::{Client, ServerEnv, spawn_server_process};
+
+fn run_route_del_keys(append_only: bool, num_keys: u32, duration: Duration) -> anyhow::Result<()> {
+    // GIVEN
+    let env = ServerEnv::default().with_append_only(append_only);
+    let process = spawn_server_process(&env)?;
+
+    let env2 = ServerEnv::default().with_append_only(append_only);
+    let process2 = spawn_server_process(&env2)?;
+
+    let mut h = Client::new_with_cluster_mode(process.port, true);
+    let mut h2 = Client::new_with_cluster_mode(process2.port, true);
+
+    for key in 0..num_keys {
+        if key % 2 == 0 {
+            h.send_and_get(format!("SET {key} {key}"));
+        }
+        if key % 2 == 1 {
+            h2.send_and_get(format!("SET {key} {key}"));
+        }
+    }
+
+    // WHEN cluster meet
+    h.send_and_get(format!("CLUSTER MEET {} eager", process2.bind_addr()));
+    sleep(duration);
+
+    // THEN Fire keys command: both handlers should get the same keys
+    let joined_keys = (0..num_keys).map(|k| format!("{k}")).collect::<Vec<_>>().join(" ");
+    let command = format!("DEL {}", joined_keys);
+
+    let res = h.send_and_get(command.clone());
+    assert_eq!(res, format!("(integer) {}", num_keys));
+    let res2 = h2.send_and_get(command);
+    assert_eq!(res2, format!("(integer) {}", 0));
+
+    Ok(())
+}
+
+#[test]
+fn run_route_del() -> anyhow::Result<()> {
+    run_route_del_keys(true, 4, Duration::from_millis(200))?;
+    run_route_del_keys(false, 4, Duration::from_millis(200))?;
+    run_route_del_keys(true, 100, Duration::from_millis(500))?;
+    run_route_del_keys(false, 100, Duration::from_millis(500))?;
+
+    Ok(())
+}
