a9dffc8dea6e78e0bc5333464ff753bfd9e8e5a0
Migo <saka90030@gmail.com>
2025-08-12 22:46:39 +0400

    shared log specific to test

--- duva/src/lib.rs
+++ duva/src/lib.rs
@@ -220,57 +220,58 @@ impl StartUpFacade {
     }
 }
 
-fn setup_tracing_shared_file() -> Result<tracing_appender::non_blocking::WorkerGuard> {
-    // Create a custom writer that uses file locking
-    struct LockedFileWriter {
-        file: std::fs::File,
-    }
-
-    impl LockedFileWriter {
-        fn new(path: &str) -> Result<Self, std::io::Error> {
-            let file = OpenOptions::new().create(true).append(true).open(path)?;
-            Ok(Self { file })
-        }
-    }
-
-    impl Write for LockedFileWriter {
-        fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-            self.file.lock().unwrap();
-            let result = self.file.write(buf)?;
-            self.file.flush()?;
-            self.file.unlock()?; // release lock
-            Ok(result)
-        }
-        fn flush(&mut self) -> std::io::Result<()> {
-            self.file.lock().unwrap();
-            self.file.flush()?;
-            self.file.unlock().unwrap();
-            Ok(())
-        }
-    }
-    let writer = LockedFileWriter::new("./shared.log")?;
-    let (non_blocking, guard) = non_blocking(writer);
-
-    let file_layer = fmt::layer()
-        .with_writer(non_blocking)
-        .with_target(true)
-        .with_line_number(true)
-        .with_file(true)
-        .with_timer(UtcTime::rfc_3339()) // nice timestamps
-        .with_ansi(true)
-        .json(); // JSON format is easier to parse from multiple processes
-
+fn setup_tracing_shared_file() -> Result<Option<tracing_appender::non_blocking::WorkerGuard>> {
     let console_layer = fmt::layer()
         .with_writer(std::io::stderr)
         .with_target(true)
         .with_line_number(true)
         .with_ansi(true);
 
-    Registry::default()
-        .with(file_layer.with_filter(ENV.log_level))
-        .with(console_layer.with_filter(ENV.log_level))
-        // .with(console_layer.with_filter(LevelFilter::INFO))
-        .init();
+    let subscriber = Registry::default().with(console_layer.with_filter(ENV.log_level));
 
-    Ok(guard)
+    let is_test_env = std::env::var("DUVA_ENV").unwrap_or_default() == "test";
+    if is_test_env {
+        // Create a custom writer that uses file locking
+        struct LockedFileWriter {
+            file: std::fs::File,
+        }
+        impl LockedFileWriter {
+            fn new(path: &str) -> Result<Self, std::io::Error> {
+                let file = OpenOptions::new().create(true).append(true).open(path)?;
+                Ok(Self { file })
+            }
+        }
+
+        impl Write for LockedFileWriter {
+            fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+                self.file.lock().unwrap();
+                let result = self.file.write(buf)?;
+                self.file.flush()?;
+                self.file.unlock()?; // release lock
+                Ok(result)
+            }
+            fn flush(&mut self) -> std::io::Result<()> {
+                self.file.lock().unwrap();
+                self.file.flush()?;
+                self.file.unlock().unwrap();
+                Ok(())
+            }
+        }
+        let writer = LockedFileWriter::new("./shared.log")?;
+        let (non_blocking, guard) = non_blocking(writer);
+        let file_layer = fmt::layer()
+            .with_writer(non_blocking)
+            .with_target(true)
+            .with_line_number(true)
+            .with_file(true)
+            .with_timer(UtcTime::rfc_3339()) // nice timestamps
+            .with_ansi(true)
+            .json(); // JSON format is easier to parse from multiple processes
+        subscriber.with(file_layer.with_filter(ENV.log_level)).init();
+        Ok(Some(guard))
+    } else {
+        subscriber.init();
+
+        Ok(None)
+    }
 }
