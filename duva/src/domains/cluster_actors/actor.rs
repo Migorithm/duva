358249c997ae90975c449d96a35bf85dc0490dd7
Migo <saka90030@gmail.com>
2025-09-03 22:26:22 +0400

    remove cmt

--- duva/src/domains/cluster_actors/actor.rs
+++ duva/src/domains/cluster_actors/actor.rs
@@ -841,8 +841,6 @@ impl<T: TWriteAheadLog> ClusterActor<T> {
             return;
         }
 
-        // * Increase the high water mark
-        // ! Revisit this logic! con_idx should be updated only after leader learns the average match index of followers
         self.increase_con_idx();
         self.client_sessions.set_response(voting.session_req.take());
 
